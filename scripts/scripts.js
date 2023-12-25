b20c03e2cf7a54979c50a00b2191f41a0fd19935
TheDuke76 <153953292+TheDuke76@users.noreply.github.com>
2023-12-25 04:19:00 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,6 +13,7 @@
     modal.style.display = 'flex'; // or 'block' for display: block;
     const parentElement = this.parentNode;
     const imgElement = this.parentNode.querySelector('img');
+    const imgSrc = imgElement.src;
     console.log('Image source:', imgSrc);
     document.getElementById('modal-image').src = imgSrc;
   }
