a90b55b869cbf3ec6117eabbb2353b1309f9f12c
TheDuke76 <153953292+TheDuke76@users.noreply.github.com>
2023-12-25 01:47:37 -0700

    header

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,9 @@
-const sliderEl = document.querySelector(".slider");
-const sliderEl2 = document.querySelector(".slider-2");
-const slider = new A11YSlider(sliderEl);
-const slider2 = new A11YSlider(sliderEl2);
+<script>
+  const clickableElements = document.querySelectorAll('.js-toggle');
 
-sliderEl.addEventListener("beforeChange", function(e) {
-  // Get next element
-  const nextSlide = e.detail.nextSlide;
-  // Get index of the next element
-  const nextSlideIndex = Array.from(nextSlide.parentNode.children).indexOf(
-    nextSlide
-  );
-  // Tell 2nd slider to move to slide element based off index
-  slider2.scrollToSlide(nextSlideIndex);
-});
+  clickableElements.forEach(function(element) {
+    element.addEventListener('click', function() {
+      this.classList.toggle('open');
+    })
+  });
+</script>
