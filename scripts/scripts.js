557627b1e0486ddbe1db5d5f37e8d2da818ee4c5
TheDuke76 <153953292+TheDuke76@users.noreply.github.com>
2023-12-31 00:13:59 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,11 +30,10 @@
   function openModalExt() {
     const modal = document.getElementById('myModal');
     modal.style.display = 'flex'; // or 'block' for display: block;
-    const linkTarget = this.href;
-    const exitText = linkTarget ? linkTarget.textContent.trim() : null;   
-    const writeExitText = document.createTextNode(exitText);
+    const linkTarget = this.href; 
+    const exitText = document.createTextNode(linkTarget);
     document.getElementById('js-target').href = linkTarget;
-    document.getElementById('js-ext-target').appendChild(writeExitText);
+    document.getElementById('js-ext-target').appendChild(exitText);
   }
 
   // Function to close the modal
